2948a7c588b0c9c7431047895bfa5c462da3e9ea
hkgnp <ben@pngs.cc>
2022-02-27 23:29:00 +0800

    added function to toggle append using ctrl/cmd + a

--- src/App.tsx
+++ src/App.tsx
@@ -154,7 +154,7 @@ const App = () => {
           />
           <div className="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
           <span className="ml-3 text-gray-900 text-sm font-medium">
-            Append TODO to Item
+            Append TODO to Item (ctrl/cmd + a)
           </span>
         </label>
 
